02e7b48f191ae1af65c51e25b1f675f37ce7326f
Teagan Clayton <clay0063@algonquinlive.com>
2023-09-25 23:52:13 -0400

    Set up basic fetching

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+import { useState } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button data={data} setData={setData} responseData={data} isLoading={false} />
+        <button onClick={()=>{console.log(data.titles)}}>View Data</button>
+        {/* {!data ? null : <p>{data.title}</p>}
+        {!data ? null : <p>{data.rating}</p>} */}
       </header>
     </div>
   );
---
+++ src/components/Button.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+
+function Button(props) {
+  const [isLoading, setIsLoading] = useState(false);
+  const url = `https://api.jikan.moe/v4/anime`;
+  let filters = `?rating=rx`;
+  let filteredURL = url + filters
+  let pageNumber = 1;
+
+
+  useEffect(() => {
+    console.log(props.data); // Log the data array when it changes
+  }, [props.data]);
+
+  function getRandom() {
+    setIsLoading(true);
+    fetch(filteredURL)
+      .then((resp) => resp.json())
+      .then((responseData) => {
+        pageNumber = responseData.pagination.last_visible_page;
+        getData();
+      })
+      .catch(() => {
+        console.warn();
+        setIsLoading(false);
+      });
+  }
+
+  function getData(){
+    let fetchURL = `${filteredURL}&page=${Math.floor(Math.random() * pageNumber)}`
+    fetch(fetchURL)
+      .then((resp) => resp.json())
+      .then((responseData) => {
+        let index = Math.floor(Math.random() * 24);
+        props.setData(responseData.data[index])
+        setIsLoading(false);
+      })
+      .catch(() => {
+        console.warn();
+        setIsLoading(false);
+      });
+  }
+
+
+
+  return (
+    <div>
+      <button onClick={getRandom} disabled={isLoading}>
+        {isLoading ? 'Loading...' : 'Click to Fetch Data'}
+      </button>
+    </div>
+  );
+}
+
+export default Button;
